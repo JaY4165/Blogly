b84de4a9c7a9a2ba8d5836d2ffb046b5022be3ab
JaY4165 <harsha.gontu@gmail.com>
2023-07-23 18:04:56 +0530

    navbar custom styles

--- components/ui/themeToggleButton.tsx
+++ components/ui/themeToggleButton.tsx
@@ -5,12 +5,6 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { HoverCard } from "@radix-ui/react-hover-card";
 import { HoverCardContent, HoverCardTrigger } from "./hover-card";
 
@@ -22,7 +16,7 @@ export function ModeToggle() {
       <HoverCardTrigger asChild className="dark:outline-none dark:border-none">
         <Button
           size="icon"
-          className="dark:outline-none dark:hover:bg-none dark:focus:bg-none dark:border-none"
+          className="dark:hover:bg-black/30 dark:hover:border-none dark:outline-none"
         >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
@@ -33,13 +27,25 @@ export function ModeToggle() {
         align="end"
         className="flex flex-col w-auto space-y-2 rounded-2xl dark:bg-black/30 dark:border-none dark:outline-none"
       >
-        <Button variant={"ghost"} onClick={() => setTheme("light")}>
+        <Button
+          variant={"ghost"}
+          onClick={() => setTheme("light")}
+          className="dark:hover:bg-black/30"
+        >
           Light
         </Button>
-        <Button variant={"ghost"} onClick={() => setTheme("dark")}>
+        <Button
+          variant={"ghost"}
+          onClick={() => setTheme("dark")}
+          className="dark:hover:bg-black/30"
+        >
           Dark
         </Button>
-        <Button variant={"ghost"} onClick={() => setTheme("system")}>
+        <Button
+          variant={"ghost"}
+          onClick={() => setTheme("system")}
+          className="dark:hover:bg-black/30"
+        >
           System
         </Button>
       </HoverCardContent>
