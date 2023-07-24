8466f916642771e3f58c89571f05f2150dec542f
JaY4165 <harsha.gontu@gmail.com>
2023-07-25 00:30:35 +0530

    blog main image

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import wallbg from "../public/wallbg.jpg";
 
 export default function Home() {
   return (
-    <main className="h-screen flex items-center flex-col pt-20 px-10">
+    <main className="h-screen flex items-center flex-col pt-20 px-5 sm:px-10 ">
       <h3 className="text-lg font-normal tracking-wider pb-2">The blog</h3>
       <div className="flex flex-col items-center">
         {/* <Image
@@ -18,23 +18,27 @@ export default function Home() {
           The latest industry news, interviews, technologies, and resources.
         </p>
       </div>
-      <div className="w-full h-full pt-8">
+      <div className="relative pt-8 ">
         <Image
           src={wallbg}
           alt="background"
           className="rounded-xl"
           loading="lazy"
         />
-        <div className="flex flex-col absolute top-full pl-10 space-y-3">
+        <div className="flex flex-col absolute top-[50%] pl-8 max-md:pl-3 md:top-[70%] space-y-1 md:space-y-3">
           <div className="flex flex-row space-x-8">
-            <h3 className="text-white">Guy Ritchie</h3>
+            <h3 className="text-white text-sm max-md:text-xs">Guy Ritchie</h3>
             <ul>
-              <li className="list-disc text-white">14 July 2023</li>
+              <li className="list-disc text-white text-sm max-md:text-xs">
+                14 July 2023
+              </li>
             </ul>
           </div>
-          <div className="flex flex-col space-y-3">
-            <h1 className="font-semibold text-2xl">UX reviews presentations</h1>
-            <p className="text-sm font-light ">
+          <div className="flex flex-col space-y-1 md:space-y-3">
+            <h1 className="font-semibold text-lg md:text-2xl text-white">
+              UX reviews presentations
+            </h1>
+            <p className="text-xs pr-2 pt-1 md:text-sm font-light  text-white">
               How do you create compelling presentations that wow your
               colleagues and impress your managers?
             </p>
