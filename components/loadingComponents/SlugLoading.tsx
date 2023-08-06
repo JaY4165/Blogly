66a5a9d01e9bbed52216c9d31093aa49f9fb83db
JaY4165 <harsha.gontu@gmail.com>
2023-08-06 10:57:47 +0530

    loading and build and deployment

--- components/loadingComponents/SlugLoading.tsx
+++ components/loadingComponents/SlugLoading.tsx
@@ -3,31 +3,17 @@ import { Skeleton } from "../ui/skeleton";
 
 const SlugLoading: React.FC = () => {
   return (
-    <div className="px-5 sm:px-10 pt-20">
+    <div className="px-5 sm:px-10 pt-20 pb-10">
       <div className="flex justify-between w-full">
-        <div className="flex pb-4 pl-1 items-center">
-          <Skeleton className="h-4 w-80" />
-          <Skeleton className="h-4 w-80" />
+        <div className="flex pb-4 pl-1 space-x-2 items-center">
+          <Skeleton className="h-10 w-10 rounded-full" />
+          <Skeleton className="pl-2 text-lg h-4 w-32" />
         </div>
         <div className="self-center pr-1">
-          <Skeleton className="h-4 w-80" />
+          <Skeleton className="h-4 w-44" />
         </div>
       </div>
-
-      <Skeleton className="rounded-xl h-[100%] w-[100%]" />
-      <div className="flex pt-5 items-center justify-start flex-row space-x-2">
-        {[...Array(6)].map(
-          (_, index: any): React.JSX.Element => (
-            <div key={index}>
-              <Skeleton className="h-7 w-7 rounded-full" />
-            </div>
-          )
-        )}
-      </div>
-
-      <section className="pb-14">
-        <Skeleton className="h-4 w-28" />
-      </section>
+      <Skeleton className="h-screen w-full rounded-xl" />
     </div>
   );
 };
