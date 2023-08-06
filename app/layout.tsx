1cd96ecbc452277b8968fa9d7c541f40a9c16e06
JaY4165 <harsha.gontu@gmail.com>
2023-08-06 23:49:26 +0530

    firebase auth removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import Navbar from "@/components/Navbar";
 import FooterComp from "@/components/FooterComp";
-import { AuthContextProvider } from "@/context/AuthContext";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,15 +17,15 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <AuthContextProvider>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <section className="px-1 lg:px-32">
-              <Navbar />
-              {children}
-            </section>
-            <FooterComp />
-          </ThemeProvider>
-        </AuthContextProvider>
+        {/* <AuthContextProvider> */}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <section className="px-1 lg:px-32">
+            <Navbar />
+            {children}
+          </section>
+          <FooterComp />
+        </ThemeProvider>
+        {/* </AuthContextProvider> */}
       </body>
     </html>
   );
--- components/LogInOutBtn.tsx
+++ components/LogInOutBtn.tsx
@@ -2,29 +2,14 @@
 
 import React, { useContext } from "react";
 import { Button } from "./ui/button";
-import AuthContext, { AuthContextProps } from "@/context/AuthContext";
 
 const LogInOutBtn = () => {
-  const AuthController = useContext<AuthContextProps | null>(AuthContext);
-
-  const handleSignIn = async () => {
-    try {
-      if (AuthController?.user) {
-        AuthController?.logOut();
-      } else {
-        AuthController?.googleSignIn();
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <Button
-      onClick={handleSignIn}
+      //   onClick={handleSignIn}
       className="bg-black text-white dark:bg-white dark:text-black outline-none border-none dark:hover:bg-white hover:bg-black hover:text-white"
     >
-      {AuthController?.user ? "Logout" : "Login"}
+      {true ? "Logout" : "Login"}
     </Button>
   );
 };
